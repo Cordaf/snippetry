29992adcd3bd5c6c70918aee6972a83c3277667c
Maxim Chelyshev <maxim.chelyshev@grse.de>
2019-04-29 12:25:47 +0300

    updates, etc

--- anchored/maincpp.cpp
+++ anchored/maincpp.cpp
@@ -3,101 +3,106 @@
 #include <functional>
 #include <vector>
 #include <unordered_map>
+#include <type_traits>
 
-#define TEXTIFY(A) #A
+namespace misc {
 
-#define INVOKABLE_FUNCTION(funcname, funcbody)      \
-struct funcname : anchored_base<funcname> {         \
-inline static const char* name() {                  \
-    return TEXTIFY(funcname);                       \
-}                                                   \
-static void call() funcbody                         \
-private:                                            \
-    inline void init() {                            \
-        anchored_base<funcname>::init;              \
-    }                                               \
-};                                                  \
+struct anchor {};
 
+namespace detail {
+    template<typename TVal>
+    struct static_storage {
+        template<typename... TArgs>
+        static decltype(auto) get(TArgs... args) {
+            static TVal val{ args... };
+            return (val);
+        }
+    };
 
-template<typename TVal>
-struct static_storage {
-    //static decltype(auto) get() {
-    //    static TVal val;
-    //    return (val);
-    //}
+    template<typename> struct deduce_type;
 
-    template<typename... TArgs>
-    static decltype(auto) get(TArgs... args) {
-        static TVal val { args... };
-        return (val);
-    }
-};
+    template<typename TOut, typename TClass, typename... TIn>
+    struct deduce_type<TOut(TClass::*)(TIn...) const> {
+        using type = std::function<TOut(TIn...) >;
+    };
 
-struct anchored {};
+    template<typename, typename...> struct anchored_function;
 
-template<typename, typename...>
-struct anchored_function;
+    template<typename TOut, typename... TIn>
+    struct anchored_function<TOut(TIn...)> : anchor {
+        using func_type = std::function<TOut(TIn...)>;
+        anchored_function(std::string name, func_type f) {
+            auto& container = static_storage<std::unordered_map<std::string, func_type>>::get();
+            container.insert(std::make_pair(name, f));
+        }
+    };
+}
 
-template<typename TOut, typename... TIn>
-struct anchored_function<TOut(TIn...)> : anchored {
-    using func_type = std::function<TOut(TIn...)>;
-    anchored_function(std::string name, func_type f) {
-        static_storage<std::unordered_map<std::string, func_type>>::get().insert(std::make_pair(name, f));
-    }
-};
+template <typename TFunc>
+auto make_function(const TFunc& fn) {
+    return typename detail::deduce_type<decltype(&TFunc::operator())>::type(fn);
+}
 
 template<typename Tag, typename TOut, typename... TIn>
-decltype(auto) make_anchored(std::string name, std::function<TOut(TIn...)> f) {
-    return std::move(static_storage<anchored_function<TOut(TIn...)>>::get(name, f));
+decltype(auto) make_anchored(std::string name, std::function<TOut(TIn...)> fn) {
+    return (detail::anchored_function<TOut(TIn...)> { name, fn });
 }
 
 template<typename Func>
 struct anchored_base {
-    static anchored init;
+    static anchor init;
+
+private:
+    static decltype(auto) do_init() {
+        return make_anchored<Func>(Func::name(), make_function(Func::function_body()));
+    }
 };
 
-template<typename Func>
-anchored anchored_base<Func>::init = make_anchored<Func>(Func::name(), std::function<decltype(Func::call)>(&Func::call));
+template<typename Func> anchor anchored_base<Func>::init = anchored_base<Func>::do_init();
 
 template<typename TSignature, typename... TIn>
 decltype(auto) invoke(std::string name, TIn... args) {
     using func_type = std::function<TSignature>;
-    auto& reg = static_storage<std::unordered_map<std::string, func_type>>::get();
+    auto& reg = detail::static_storage<std::unordered_map<std::string, func_type>>::get();
     if (reg.find(name) == reg.end())
         throw std::runtime_error("function not found");
     return reg[name](args...);
 }
 
+#define INVOKABLE_FUNCTION_(funcname, funcbody)     \
+class funcname : misc::anchored_base<funcname> {    \
+    using base_type = misc::anchored_base<funcname>;\
+    inline void init() { base_type::init; }         \
+    friend base_type;                               \
+    inline static const char* name() noexcept {     \
+        return #funcname; };                        \
+    static decltype(auto) function_body() noexcept {\
+        return funcbody;                            \
+    }                                               \
+};                                                  \
 
-struct foo : anchored_base<foo> {
-    inline static const char* name() { return "foo"; };
+}//misc
 
-    static void call() {
-        std::cout << "bang!" << std::endl;
+class bar : misc::anchored_base<bar> {
+    using base_type = misc::anchored_base<bar>;
+    friend base_type;
+    inline void init() { base_type::init; }
+
+    inline static const char* name() noexcept { return "bar"; };
+
+    static decltype(auto) function_body() noexcept {
+        return [](int i) { std::cout << __FUNCTION__ << " " << i << std::endl; };
     }
-
-private:
-    inline void init() { anchored_base<foo>::init; }
 };
 
-struct bar : anchored_base<bar> {
-    inline static const char* name() { return "bar"; };
-
-    static void call(int i) {
-        std::cout << __FUNCTION__ << " " << i << std::endl;
-    }
-
-private:
-    inline void init() { anchored_base<bar>::init; }
-};
-
-INVOKABLE_FUNCTION(baz, { std::cout << "baz!" << std::endl; })
+INVOKABLE_FUNCTION_(foo, []() { std::cout << __FUNCTION__ << std::endl; })
+INVOKABLE_FUNCTION_(baz, [](int i) { std::cout << __FUNCTION__ << " " << i << std::endl; })
 
 int main() {
-    invoke<void(int)>("bar", 42);
-    //invoke<void(void)>("bar");
+    misc::invoke<void(void)>("foo");
+    misc::invoke<void(int)>("bar", 42);
+    misc::invoke<void(int)>("baz", 43);
 
-    invoke<void(void)>("foo");
-    invoke<void(void)>("baz");
+    //invoke<void(void)>("bar");
     //invoke<void(int)>("foo", 42);
 }
